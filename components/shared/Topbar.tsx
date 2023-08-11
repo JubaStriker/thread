e6fa4581c96dd9695381b798c270b71a27b1cb93
JubaStriker <jubastriker@gmail.com>
2023-08-11 14:39:25 +0600

    Icons added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Topbar = () => {
     return (
-        <h1>
-            Topbar
-        </h1>
+        <nav className='topbar'>
+            <Link href='/' className='flex item-center gap-4'>
+                <Image src='/assets/logo.svg' alt='logo' height={28} width={28} />
+                <p className='text-heading-3-bold text-light-1 max-xs:hidden'>Threads</p>
+            </Link>
+        </nav>
     );
 };
 
