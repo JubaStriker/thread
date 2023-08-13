0565f8f6b9549a2a08e061e24ba96ace607c223c
JubaStriker <jubastriker@gmail.com>
2023-08-13 15:31:20 +0600

    Topbar added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -9,7 +10,33 @@ const Topbar = () => {
                 <Image src='/assets/logo.svg' alt='logo' height={28} width={28} />
                 <p className='text-heading-3-bold text-light-1 max-xs:hidden'>Threads</p>
             </Link>
-        </nav>
+
+            <div className='flex items-center gap-1'>
+                <div className='block md:hidden '>
+                    {/* <SignedIn>
+
+                    </SignedIn> */}
+                    <SignOutButton>
+                        <div className='flex cursor-pointer'>
+                            <Image
+                                src='/assets/logout.svg'
+                                alt='logout'
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                    </SignOutButton>
+                </div>
+
+                <OrganizationSwitcher
+                    appearance={{
+                        elements: {
+                            organizationSwitcherTrigger: 'py-2 px-4'
+                        }
+                    }} />
+
+            </div>
+        </nav >
     );
 };
 
