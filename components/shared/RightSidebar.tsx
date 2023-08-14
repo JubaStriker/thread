be2077db500a4dfbe086bab3b0b79da3ad1c031a
JubaStriker <jubastriker@gmail.com>
2023-08-14 20:03:09 +0600

    Right side bar initiated

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-
 const RightSidebar = () => {
     return (
-        <h1>
-            RightSidebar
-        </h1>
+
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start ">
+                <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+            </div>
+            <div className="flex flex-1 flex-col justify-start ">
+                <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+            </div>
+        </section>
     );
 };
 
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import { dark } from '@clerk/themes'
 
 const Topbar = () => {
     return (
@@ -30,6 +30,7 @@ const Topbar = () => {
 
                 <OrganizationSwitcher
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger: 'py-2 px-4'
                         }
