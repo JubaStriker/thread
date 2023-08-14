1a5d75e7950d24e294dcff3689e756ead5b85d12
JubaStriker <jubastriker@gmail.com>
2023-08-14 20:59:40 +0600

    Form UI initialized

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,11 +1,19 @@
 "use client"
+import { Button } from "@/components/ui/button"
 import {
-    Form
-} from '@/components/ui/form';
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserValidation } from '@/lib/validations/user';
-
+import * as z from "zod"
 
 interface Props {
     user: {
@@ -31,8 +39,34 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             bio: ''
         }
     });
+
+    function onSubmit(values: z.infer<typeof UserValidation>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
-        <Form>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
         </Form>
     )
 }
