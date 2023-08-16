1a994b17161a0cf2400d7bb0afd2cf07bc3fc4fa
JubaStriker <jubastriker@gmail.com>
2023-08-16 23:43:28 +0600

    Backend setup initiated @2.01

---
+++ lib/mongoose.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; //To check if connected to DB
+
+export const connectToDb = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+    if (isConnected) return console.log("Already connected to MONGODB");
+
+    try { }
+    catch (error) { }
+}
