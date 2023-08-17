6be177517f89645bbe9731570507bdda19d5c294
JubaStriker <jubastriker@gmail.com>
2023-08-17 15:14:00 +0600

    User schema decleared

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,1 +1,7 @@
 "use server"
+
+import { connectToDb } from "../mongoose"
+
+export async function updateUser(): Promise<void> {
+    connectToDb();
+}
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -8,6 +8,12 @@ export const connectToDb = async () => {
     if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
     if (isConnected) return console.log("Already connected to MONGODB");
 
-    try { }
-    catch (error) { }
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        isConnected = true;
+        console.log("Connected to mongodb");
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
