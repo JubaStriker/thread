ea85e51d68e5ee33306268116049cf48d0b669de
JubaStriker <jubastriker@gmail.com>
2023-08-19 13:39:55 +0600

    Fetch post function added

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -3,7 +3,6 @@ import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDb } from "../mongoose"
-import { redirect } from "next/navigation";
 
 interface Params {
     text: string,
@@ -36,4 +35,34 @@ export async function createThread({ text, author, communityId, path }: Params) 
     catch (error: any) {
         throw new Error(`Error creating thread: ${error.message}`)
     }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDb();
+
+    // Calculate the number of of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Only fetch the top level posts that have no parents.
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
 }
