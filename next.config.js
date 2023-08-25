8716f8854cefe84a29c680c5059bda99f700949e
JubaStriker <jubastriker@gmail.com>
2023-08-26 01:03:09 +0600

    Ignored build errors for now in next config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+        ignoreDevErrors: true,
+    },
     experimental: {
         serverActions: true,
         serverComponentsExternalPackages: ["mongoose"],
